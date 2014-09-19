d22f95ef0ed1b1e6b93349d83fa87d07325c67f2
John A. Huebner II <Hube2@users.noreply.github.com>
2014-09-18 21:54:27 -0400

    First commit of duplicator

---
+++ fieldset-duplicator.php
@@ -0,0 +1,61 @@
+<?php 
+	
+	// If this file is called directly, abort.
+	if (!defined('WPINC')) {die;}
+	
+	new acfOptionsPagefieldsetDuplicator();
+	
+	class acfOptionsPagefieldsetDuplicator {
+		
+		private $post_type = 'acf-opt-grp-dup';
+		
+		public function __construct() {
+			add_action('acf_options_page/init', array($this, 'init'));
+		} // end public function __construct
+		
+		public function init() {
+			$this->register_post_type();
+		} // end public funtion init
+		
+		private function register_post_type() {
+			$options_page_post_type = apply_filters('acf_options_page/post_type', false);
+			$text_domain = apply_filters('acf_options_page/text_domain', false);
+			if (!$options_page_post_type === false || !$text_domain === false) {
+				return;
+			}
+      $args = array('label' => __('Field Group Duplicators', $text_domain),
+										'singular_label' => __('Field Group Duplicator', $text_domain),
+                    'description' => '',
+                    'public' => false,
+										'has_archive' => false,
+                    'show_ui' => true,
+                    'show_in_menu' => 'edit.php?post_type='.$options_page_post_type,
+                    'capability_type' => 'post',
+                    'map_meta_cap' => true,
+                    'hierarchical' => false,
+                    'rewrite' => array('slug' => $this->post_type, 'with_front' => true),
+                    'query_var' => $this->post_type,
+                    'exclude_from_search' => true,
+                    'menu_position' => false,
+										//'menu_icon' => 'dashicons-admin-generic',
+                    'supports' => array('title','custom-fields','revisions'),
+                    'labels' => array('name' => __('Options Page Field Group Duplicators', $text_domain),
+                                      'singular_name' => __('Field Group Duplicator', $text_domain),
+                                      'menu_name' =>  __('Field Group Duplicators', $text_domain),
+                                      'add_new' => __('Add Field Group Duplicator', $text_domain),
+                                      'add_new_item' => __('Add New Field Group Duplicator', $text_domain),
+                                      'edit' => __('Edit', $text_domain),
+                                      'edit_item' => __('Edit Field Group Duplicator', $text_domain),
+                                      'new_item' => __('New Field Group Duplicator', $text_domain),
+                                      'view' => __('View Field Group Duplicator', $text_domain),
+                                      'view_item' => __('View Field Group Duplicator', $text_domain),
+                                      'search_items' => __('Search Field Group Duplicators', $text_domain),
+                                      'not_found' => __('No Field Group Duplicators Found', $text_domain),
+                                      'not_found_in_trash' => __('No Field Group Duplicators Found in Trash', $text_domain),
+                                      'parent' => __('Parent Field Group Duplicators', $text_domain)));
+			register_post_type($this->post_type, $args);
+		} // end private function register_post_type
+		
+	} // end class acfOptionsPagefieldsetDuplicator
+	
+?>
